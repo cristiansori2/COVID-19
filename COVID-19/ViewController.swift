7d661c4cfad8bff3b4dd957179e2228bed6274c6
Cristian Sori <cristiansori2@gmail.com>
2020-03-26 15:16:50 -0700

    Initial Commit

---
+++ COVID-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID-19
+//
+//  Created by Cristian Sori on 3/26/20.
+//  Copyright © 2020 Cristian Sori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
